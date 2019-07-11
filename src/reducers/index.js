5cda1d367d22247480b08e54d72a80058a825f5d
Tara Sherman <taradelin@gmail.com>
2019-07-10 20:19:03 -0400

    action creator functions and reducers created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,7 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (state = initialState, action) => {
+export default function(state = initialState, action) {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
